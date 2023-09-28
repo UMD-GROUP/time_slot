644f2537db66bea1fd53ff5c5bd1595deb758f48
ahadjonovss <samandarahadjonov@gmail.com>
2023-09-28 14:42:11 +0500

    main page done

--- lib/ui/user/orders/ui/ordersPage.dart
+++ lib/ui/user/orders/ui/ordersPage.dart
@@ -7,7 +7,7 @@ class OrdersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Orders Page"),
+        title: const Text("Orders Page"),
       ),
     );
   }
