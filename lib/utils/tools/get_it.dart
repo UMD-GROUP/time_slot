743e5cb19e97e1642f71500337194d4f83444ace
ahadjonovss <samandarahadjonov@gmail.com>
2023-10-10 09:19:27 +0500

    fixing

--- lib/utils/tools/get_it.dart
+++ lib/utils/tools/get_it.dart
@@ -1,9 +1,19 @@
+import 'package:time_slot/ui/admin/admin_home/data/repository/admin_repository.dart';
 import 'package:time_slot/utils/tools/file_importers.dart';
 
 final getIt = GetIt.instance;
 
 void setup() {
-   FirebaseFirestore firebaseFirestore;
-  getIt.registerLazySingleton(() => AuthorizationRepository());
-  getIt.registerLazySingleton(() => AdvertisementRepository(FirebaseFirestore.instance));
+  final FirebaseFirestore fireStoreInstance = FirebaseFirestore.instance;
+  getIt
+    ..registerLazySingleton(AuthorizationRepository.new)
+    ..registerLazySingleton(UserRepository.new)
+    ..registerLazySingleton(() => DataFromAdminRepository(fireStoreInstance))
+    // ignore: avoid_single_cascade_in_expression_statements
+    ..registerLazySingleton(() => PurchaseRepository(fireStoreInstance))
+    ..registerLazySingleton(CreateOrderRepository.new)
+    ..registerLazySingleton(() => OrdersRepository(fireStoreInstance))
+    ..registerLazySingleton(() => UsersRepository(fireStoreInstance))
+    ..registerLazySingleton(() => UserAccountRepository(fireStoreInstance))
+    ..registerLazySingleton(() => AdminRepository(fireStoreInstance));
 }
