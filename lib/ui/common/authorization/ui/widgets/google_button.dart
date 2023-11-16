06552dabe64192a3781f9d788d585a1d5cda8935
ahadjonovss <samandarahadjonov@gmail.com>
2023-11-16 19:59:24 +0500

    stores has been seperated

--- lib/ui/common/authorization/ui/widgets/google_button.dart
+++ lib/ui/common/authorization/ui/widgets/google_button.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:io';
+
 import '../../../../../utils/tools/file_importers.dart';
 
 class GoogleButton extends StatelessWidget {
@@ -7,41 +9,44 @@ class GoogleButton extends StatelessWidget {
   bool isSignIn;
 
   @override
-  Widget build(BuildContext context) => OnTap(
-        onTap: () async {
-          if (isSignIn) {
-            context
-                .read<AuthorizationBloc>()
-                .add(CreateAccountWithGoogleEvent(true));
-          } else {
-            context
-                .read<AuthorizationBloc>()
-                .add(CreateAccountWithGoogleEvent(false));
-          }
-        },
-        child: Container(
-          padding: EdgeInsets.all(12.h),
-          width: width(context) * 0.6,
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.grey),
-            borderRadius: BorderRadius.circular(10.h),
-            // color: AdaptiveTheme.of(context).theme.hintColor
-          ),
-          child: Row(
-            children: <Widget>[
-              Image.asset(
-                AppImages.google, // Provide your Google logo image asset
-                height: 36, // Adjust the height as needed
-              ),
-              const SizedBox(width: 12), // Spacing between the icon and text
-              Text(
-                'sign_in_with_google'.tr,
-                style: TextStyle(
-                  color: Colors.black, // Text color
-                  fontSize: 18.sp, // Text font size
+  Widget build(BuildContext context) => Visibility(
+        visible: Platform.isAndroid,
+        child: OnTap(
+          onTap: () async {
+            if (isSignIn) {
+              context
+                  .read<AuthorizationBloc>()
+                  .add(CreateAccountWithGoogleEvent(true));
+            } else {
+              context
+                  .read<AuthorizationBloc>()
+                  .add(CreateAccountWithGoogleEvent(false));
+            }
+          },
+          child: Container(
+            padding: EdgeInsets.all(12.h),
+            width: width(context) * 0.6,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey),
+              borderRadius: BorderRadius.circular(10.h),
+              // color: AdaptiveTheme.of(context).theme.hintColor
+            ),
+            child: Row(
+              children: <Widget>[
+                Image.asset(
+                  AppImages.google, // Provide your Google logo image asset
+                  height: 36, // Adjust the height as needed
                 ),
-              ),
-            ],
+                const SizedBox(width: 12), // Spacing between the icon and text
+                Text(
+                  'sign_in_with_google'.tr,
+                  style: TextStyle(
+                    color: Colors.black, // Text color
+                    fontSize: 18.sp, // Text font size
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       );
